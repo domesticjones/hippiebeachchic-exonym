0454c44caa06cac4239b77e0148bf8ec5d57127f
Dustin Jones <info@domesticjones.com>
2020-07-19 01:47:10 -0600

    Fine tune styling for mobile, grid layout, and modals

--- modules/layout-grid.php
+++ modules/layout-grid.php
@@ -5,6 +5,9 @@
   $modalContent = ex_modal_images($photos['a'], $photos['b'], '$' . $data['cost'], $id, $data['sold']);
 ?>
 <li id="modal-trigger-<?php echo $id; ?>" class="modal-trigger" style="background-image: url(<?php echo $photos['a']['sizes']['thumbnail-medium']; ?>)">
-  <?php echo ex_modal($id, $modalContent) ?>
+  <?php
+    echo wp_get_attachment_image($photos['a']['id'], 'thumbnail-medium', false, array('class' => 'layout-grid-image'));
+    echo ex_modal($id, $modalContent);
+  ?>
   <h2 class="layout-grid-id">#<?php echo $id; ?></h2>
 </li>
